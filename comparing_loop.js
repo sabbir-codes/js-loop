537a16725bfecf7b91a40755ab1cf0cbe7489e49
sabbir-codes <sabbir.ah799@gmail.com>
2026-07-24 14:52:59 +0600

    comparing all loop

---
+++ comparing_loop.js
@@ -0,0 +1,12 @@
+// Write the same task — printing numbers 1 to 5 — three times: once using for, once using while, and once using do while. Compare how the code structure differs for each.
+// for loop version
+// for(let i = 1; i<=5; i++){
+//     console.log(i);
+// }
+
+// while loop version
+let i = 1;
+while(i<=5){
+    console.log(i);
+    i++;
+}
